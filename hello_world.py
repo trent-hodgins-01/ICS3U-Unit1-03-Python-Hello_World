fc87e5bba7068ac0f4ff79e705fa6a108775d21f
Ubuntu <ubuntu@ip-172-31-12-63.ec2.internal>
2021-09-10 17:43:12 +0000

    added file

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+#!/user/bin/env python3
+
+# Created by Trent Hodgins
+# Created on 09/10/20
+# This is the "Hello, World!" program, with proper style
+
+
+def main():
+    print("Hello, World!")
+
+
+if __name__ == "__main__":
+    main()
